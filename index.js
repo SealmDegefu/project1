a35681ec01c3fa5a560855cfe3d9f6a5f78d6548
Selam Degefu <selam.tadd.degefu@gmail.com>
2021-04-19 17:11:52 -0700

    updated the dropdown

---
+++ index.js
@@ -0,0 +1,43 @@
+var closebtns = document.getElementsByClassName("close");
+var i;
+
+for (i = 0; i < closebtns.length; i++) {
+  closebtns[i].addEventListener("click", function() {
+    this.parentElement.style.display = 'none';
+  });
+// }
+
+// const EMPTY_HEART = '♡'
+// const FULL_HEART = '♥'
+
+// const likeGlyph = document.querySelectorAll('.like-glyph')
+// function likeBtn(event){
+//   const heart = event.target;
+
+//     mimicServerCall().then(function(){
+//       if(heart.innerText === EMPTY_HEART){
+//         heart.innerText = FULL_HEART
+//         heart.className = "activated-heart"
+//       }
+//       else if (like.innerText === FULL_HEART){
+//         heart.innerText = EMPTY_HEART
+//         heart.className = ""
+//       }
+//     })
+// }
+// for (const glyph of likeGlyph){
+// glyph.addEventListener('click', likeBtn);
+// }
+
+// function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+//   return new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//       let isRandomFailure = Math.random() < .2
+//       if (isRandomFailure) {
+//         reject("Random server error. Try again.");
+//       } else {
+//         resolve("Pretend remote server notified of action!");
+//       }
+//     }, 300);
+//   });
+// }
