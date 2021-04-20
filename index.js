07a25f7d0162d69786cb7c7aa1362c42e68aa0c6
jazmin <jazminsol@yahoo.com>
2021-04-20 13:03:41 -0500

    same

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var closebtns = document.getElementsByClassName("close");
 var i;
-
 for (i = 0; i < closebtns.length; i++) {
   closebtns[i].addEventListener("click", function() {
     this.parentElement.style.display = 'none';
@@ -8,7 +7,6 @@ for (i = 0; i < closebtns.length; i++) {
 }
 var slideIndex = 0;
 carousel();
-
 function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
@@ -18,22 +16,58 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 2000); 
+  setTimeout(carousel, 2000);
 }
-
-  const editButton = document.querySelector(".button1")
-  editButton.addEventListener('click', (event)=> {
-    event.preventDefault()
-    console.log("hey")
-
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => 
-
-{    addToy = !addToy;
+let addToy = false;
+document.addEventListener("DOMContentLoaded", () => {
+  const editBtn = document.querySelector(".button3");
+  const toyFormContainer = document.querySelector(".container3");
+  editBtn.addEventListener("click", (event) => {
+      event.preventDefault()
+      console.log("hey")
+    // hide & seek with the form
+    addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
     } else {
       toyFormContainer.style.display = "none";
     }
-  })
   });
+});
+let addToy2 = false;
+document.addEventListener("DOMContentLoaded", () => {
+  const editBtn = document.querySelector(".button2");
+  const toyFormContainer = document.querySelector(".container2");
+  editBtn.addEventListener("click", (event) => {
+      event.preventDefault()
+      console.log("hey")
+    // hide & seek with the form
+    addToy2 = !addToy2;
+    if (addToy2) {
+      toyFormContainer.style.display = "block";
+    } else {
+      toyFormContainer.style.display = "none";
+    }
+  });
+});
+let addToy1 = false;
+document.addEventListener("DOMContentLoaded", () => {
+  const editBtn = document.querySelector(".button1");
+  const toyFormContainer = document.querySelector(".container1");
+  editBtn.addEventListener("click", (event) => {
+      event.preventDefault()
+      console.log("hey")
+    // hide & seek with the form
+    addToy1 = !addToy1;
+    if (addToy1) {
+      toyFormContainer.style.display = "block";
+    } else {
+      toyFormContainer.style.display = "none";
+    }
+  });
+});
+const submitButton = document.querySelector(".submit1")
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault()
+  console.log('hola')
+})
