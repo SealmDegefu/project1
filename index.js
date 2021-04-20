260cccaaafcc385f5a75caff1c047feea396e82f
Selam Degefu <selam.tadd.degefu@gmail.com>
2021-04-20 13:48:30 -0700

    updated edit field

--- index.js
+++ index.js
@@ -66,8 +66,71 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
-const submitButton = document.querySelector(".submit1")
-submitButton.addEventListener('click', (event) => {
-  event.preventDefault()
-  console.log('hola')
-})
+const ul = document.querySelector(".coffee2")
+
+const submitBtn = document.querySelector(".submitListener2")
+submitBtn.addEventListener('submit', (event) =>{
+    event.preventDefault()
+    const input = document.querySelector(".input-text")
+    console.log(event.target.comment.value)
+    const newLi = document.createElement('li')
+    newLi.className = "newLi"
+    ul.appendChild(newLi)
+
+    newLi.innerText = event.target.comment.value
+    event.target.reset()
+    alert('Your Coffee will be ready in a hour')
+}
+)
+
+const ul1 = document.querySelector(".coffee1")
+const submitBtn1 = document.querySelector(".submitListener1")
+submitBtn1.addEventListener('submit', (event) =>{
+    event.preventDefault()
+    const input = document.querySelector(".input-text")
+    console.log(event.target.comment.value)
+    const newLi1 = document.createElement('li')
+    newLi1.className = "newLi1"
+    ul1.appendChild(newLi1)
+
+    newLi1.innerText = event.target.comment.value
+    event.target.reset()
+    alert('Your Coffee will be ready in a hour')
+}
+)
+
+const ul3 = document.querySelector(".coffee3")
+const submitBtn3 = document.querySelector(".submitListener3")
+submitBtn3.addEventListener('submit', (event) =>{
+    event.preventDefault()
+    // const input = document.querySelector(".input-text")
+    console.log(event.target.comment.value)
+    const newLi3 = document.createElement('li')
+    newLi3.className = "newLi3"
+    ul3.appendChild(newLi3)
+
+    newLi3.innerText = event.target.comment.value
+    event.target.reset()
+    alert('Your Coffee will be ready in a hour')
+}
+)
+
+    // let newComment = {
+    //     "imageId": 1,
+    //     "content": newLi.innerText
+    // }
+    // postURL(newComment)
+
+// const newLi = document.createElement("li")
+// newLi.className = "newLi"
+// const input = document.querySelector(".input-text")
+//   console.log(newLi.innerText = input.value)
+//   document.querySelector(".coffee2").appendChild(newLi)
+//   event.target.reset()
+
+// const submitButton = document.querySelector(".submit1")
+// submitButton.addEventListener('click', (event) => {
+//   event.preventDefault()
+//   console.log('hola')
+// })
+
