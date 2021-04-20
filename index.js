855ed48a498444b6c6b856f05b78f4c7a0c66455
Selam Degefu <selam.tadd.degefu@gmail.com>
2021-04-20 08:06:44 -0700

    updated image

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ for (i = 0; i < closebtns.length; i++) {
   closebtns[i].addEventListener("click", function() {
     this.parentElement.style.display = 'none';
   });
+}
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); 
+}
+
 // }
 
 // const EMPTY_HEART = '♡'
