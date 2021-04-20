8c36aed8ac5d81ab582fae3ca6bad1aca6e0d747
Selam Degefu <selam.tadd.degefu@gmail.com>
2021-04-20 10:58:45 -0700

    added edit option

--- index.js
+++ index.js
@@ -21,19 +21,62 @@ function carousel() {
   setTimeout(carousel, 2000); 
 }
 
-  const editButton = document.querySelector(".button1")
-  editButton.addEventListener('click', (event)=> {
-    event.preventDefault()
-    console.log("hey")
+let addToy = false;
 
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => 
-
-{    addToy = !addToy;
+document.addEventListener("DOMContentLoaded", () => {
+  const editBtn = document.querySelector(".button3");
+  const toyFormContainer = document.querySelector(".container3");
+  editBtn.addEventListener("click", (event) => {
+      event.preventDefault()
+      console.log("hey")
+    // hide & seek with the form
+    addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
     } else {
       toyFormContainer.style.display = "none";
     }
-  })
   });
+});
+
+let addToy2 = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const editBtn = document.querySelector(".button2");
+  const toyFormContainer = document.querySelector(".container2");
+  editBtn.addEventListener("click", (event) => {
+      event.preventDefault()
+      console.log("hey")
+    // hide & seek with the form
+    addToy2 = !addToy2;
+    if (addToy2) {
+      toyFormContainer.style.display = "block";
+    } else {
+      toyFormContainer.style.display = "none";
+    }
+  });
+});
+
+let addToy1 = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const editBtn = document.querySelector(".button1");
+  const toyFormContainer = document.querySelector(".container1");
+  editBtn.addEventListener("click", (event) => {
+      event.preventDefault()
+      console.log("hey")
+    // hide & seek with the form
+    addToy1 = !addToy1;
+    if (addToy1) {
+      toyFormContainer.style.display = "block";
+    } else {
+      toyFormContainer.style.display = "none";
+    }
+  });
+});
+
+const submitButton = document.querySelector(".submit1")
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault()
+  console.log('hola')
+})
