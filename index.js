e7937d89300be8ce93bb7b69e65690b12130fc3f
jazmin <jazminsol@yahoo.com>
2021-04-21 10:03:50 -0500

    same

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 const ul = document.querySelector(".coffee2")
+​
 const submitBtn = document.querySelector(".submitListener2")
 submitBtn.addEventListener('submit', (event) =>{
     event.preventDefault()
@@ -75,11 +76,13 @@ submitBtn.addEventListener('submit', (event) =>{
     const newLi = document.createElement('li')
     newLi.className = "newLi"
     ul.appendChild(newLi)
+​
     newLi.innerText = event.target.comment.value
     event.target.reset()
     alert('Your Coffee will be ready in a hour')
 }
 )
+​
 const ul1 = document.querySelector(".coffee1")
 const submitBtn1 = document.querySelector(".submitListener1")
 submitBtn1.addEventListener('submit', (event) =>{
@@ -89,42 +92,89 @@ submitBtn1.addEventListener('submit', (event) =>{
     const newLi1 = document.createElement('li')
     newLi1.className = "newLi1"
     ul1.appendChild(newLi1)
+​
     newLi1.innerText = event.target.comment.value
     event.target.reset()
     alert('Your Coffee will be ready in a hour')
 }
 )
+const url = "http://localhost:3000/drink"
+​
+fetch(url)
+.then(res => res.json())
+.then(renderComment)
 const ul3 = document.querySelector(".coffee3")
+function renderComment (displayComment)
+{
+   displayComment.forEach(element =>{
+    const newLi3 = document.createElement('li')
+    newLi3.className = "newLi3"
+    newLi3.innerText = element.comments
+    ul3.appendChild(newLi3)
+   })
+}
+​
 const submitBtn3 = document.querySelector(".submitListener3")
 submitBtn3.addEventListener('submit', (event) =>{
     event.preventDefault()
     // const input = document.querySelector(".input-text")
-    console.log(event.target.comment.value)
     const newLi3 = document.createElement('li')
     newLi3.className = "newLi3"
     ul3.appendChild(newLi3)
     newLi3.innerText = event.target.comment.value
     event.target.reset()
     alert('Your Coffee will be ready in a hour')
+    let newPatchComment = {
+        "comments": event.target.comment.value
+      }
+    post(newPatchComment)
+});
+​
+// fetch(url)
+// .then(res => res.json())
+// .then(postPreference)
+​
+// function postPreference(drinkChoices){
+//     console.log(drinkChoices)
+//    const option= document.querySelector("#coffee")
+//    option.addEventListener('submit', () =>{
+//     console.log("hey")
+//     let newOption = {
+//         "milk": drinkChoices.drinkDescription.milk
+//     }
+//     post(newOption)
+​
+//    })
+​
+​
+// }
+​
+​
+​
+function post(comment) {
+  let optionPost= {
+    method:'POST',
+    headers:{
+    'Content-Type':'application/json',
+    "Accept":'application/json'
+    },
+    body: JSON.stringify(comment),
+  }
+  fetch(url, optionPost)
+  .then(res => res.json())
+  .then(data => console.log(data))
 }
-)
-
-    // let newComment = {
-    //     "imageId": 1,
-    //     "content": newLi.innerText
-    // }
-    // postURL(newComment)
-
-// const newLi = document.createElement("li")
-// newLi.className = "newLi"
-// const input = document.querySelector(".input-text")
-//   console.log(newLi.innerText = input.value)
-//   document.querySelector(".coffee2").appendChild(newLi)
-//   event.target.reset()
-
-// const submitButton = document.querySelector(".submit1")
-// submitButton.addEventListener('click', (event) => {
-//   event.preventDefault()
-//   console.log('hola')
-// })
-
+// const patchUrl = "http://localhost:3000/drink/comments/:id" 
+// function patch(comment) { 
+//   let optionPatch= {
+//     method:'PATCH',
+//     headers:{
+//     'Content-Type':'application/json',
+//     "Accept":'application/json'
+//     },
+//     body: JSON.stringify(comment),
+//   }
+//   fetch(url, optionPatch)
+//   .then(res => res.json())
+//   .then(data => console.log(data))
+// }
